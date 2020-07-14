b26e6658b135e740eb44b66a470e6a93bbc7e428
silvanrijnberk <silvanrijnberk@live.com>
2020-07-14 11:25:45 +0200

    second commit

--- src/main/java/com/TOSAD/Reparatie/Services/BusinessRuleController.java
+++ src/main/java/com/TOSAD/Reparatie/Services/BusinessRuleController.java
@@ -1,13 +1,20 @@
 package com.TOSAD.Reparatie.Services;
 
+import com.TOSAD.Reparatie.DAO.BusinessRuleAttributesDAO;
+import com.TOSAD.Reparatie.DAO.BusinessRuleColumnsDAO;
 import com.TOSAD.Reparatie.DAO.BusinessRuleDAO;
+import com.TOSAD.Reparatie.DAO.BusinessRuleTableDAO;
 import com.TOSAD.Reparatie.Domain.BusinessRule;
+import com.TOSAD.Reparatie.Domain.BusinessRuleAttributes;
+import com.TOSAD.Reparatie.Domain.BusinessRuleColumns;
+import com.TOSAD.Reparatie.Domain.BusinessRuleTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.jar.Attributes;
 
 @RestController
 @RequestMapping("/businessRules")
@@ -16,11 +23,35 @@ public class BusinessRuleController {
 
     @Autowired
     BusinessRuleDAO businessRuleDAO;
+    @Autowired
+    BusinessRuleAttributesDAO businessRuleAttributesDAO;
+    @Autowired
+    BusinessRuleColumnsDAO businessRuleColumnsDAO;
+    @Autowired
+    BusinessRuleTableDAO businessRuleTableDAO;
 
 //    Save An BusinessRule
-    @PostMapping("")
-    public ResponseEntity<BusinessRule> createBusinessRule(@Valid @RequestBody BusinessRule businessRule) {
-        return ResponseEntity.ok().body(businessRuleDAO.save(businessRule));
+    @PostMapping("/{table}/{column}/{attributes}")
+    public ResponseEntity<BusinessRule> createBusinessRule(@Valid @RequestBody BusinessRule businessRule,@PathVariable(value = "table") String table,@PathVariable(value = "column") String column,@PathVariable(value = "attributes") String attributes) {
+
+        businessRuleDAO.save(businessRule);
+
+        BusinessRuleAttributes businessRuleAttributes = new BusinessRuleAttributes();
+        businessRuleAttributes.setAttributes(attributes);
+        businessRuleAttributes.setBusinessRule(businessRule);
+
+        BusinessRuleColumns businessRuleColumns= new BusinessRuleColumns();
+        businessRuleColumns.setColumns(column);
+        businessRuleColumns.setBusinessRule(businessRule);
+
+        BusinessRuleTable businessRuleTable = new BusinessRuleTable();
+        businessRuleTable.setTable(table);
+        businessRuleTable.setBusinessRule(businessRule);
+
+        businessRuleAttributesDAO.save(businessRuleAttributes);
+        businessRuleColumnsDAO.save(businessRuleColumns);
+        businessRuleTableDAO.save(businessRuleTable);
+        return ResponseEntity.ok().body(businessRule);
     }
 
 //    Find All BusinessRule
